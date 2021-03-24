e94099925af4ea62a2c91c65daa1b255181f7b2e
Tyler Derr <tylerderr@vanderbilt.edu>
2021-03-24 00:54:23 -0500

    index,mentoring,outreach updates

--- update_site.sh
+++ update_site.sh
@@ -11,7 +11,7 @@ mv jemdocs/*.html .
 chmod 644 *.html
 
 # Adding the clustermap to the pages
-line=38
+line=39
 gsed -i "${line}i <script type=\"text/javascript\" id=\"clustrmaps\" src=\"//cdn.clustrmaps.com/map_v2.js?d=leIDuRwlUzsESOVq6m32RqbgLChp2xkZYp22IZ7\QInU&cl=ffffff&w=a\"></script>" index.html
 gsed -i "${line}i <script type=\"text/javascript\" id=\"clustrmaps\" src=\"//cdn.clustrmaps.com/map_v2.js?d=leIDuRwlUzsESOVq6m32RqbgLChp2xkZYp22IZ7\QInU&cl=ffffff&w=a\"></script>" publications.html
 gsed -i "${line}i <script type=\"text/javascript\" id=\"clustrmaps\" src=\"//cdn.clustrmaps.com/map_v2.js?d=leIDuRwlUzsESOVq6m32RqbgLChp2xkZYp22IZ7\QInU&cl=ffffff&w=a\"></script>" presentations.html
@@ -28,11 +28,12 @@ gsed -i "${line}i <script type=\"text/javascript\" id=\"clustrmaps\" src=\"//cdn
 gsed -i "${line}i <script type=\"text/javascript\" id=\"clustrmaps\" src=\"//cdn.clustrmaps.com/map_v2.js?d=leIDuRwlUzsESOVq6m32RqbgLChp2xkZYp22IZ7\QInU&cl=ffffff&w=a\"></script>" openings.html
 
 # Adding the recruiting line
-line=75
+line=76
 gsed -i "${line}i <p><b><font color=\"red\">[Open positions]</font><br> I am recruiting</b> PhD students to work with me on topics in my general research interests (seen below). Master's and undergraduate students within VU and visiting scholars are also welcome. Please feel free to email me. <br><b>Please see <a href=\"openings.html\">here</a> for position details.</b></p>" index.html
 
 # Adding Twitter link
-gsed -i "56i <a href=\"https://twitter.com/tylersnetwork?ref_src=twsrc%5Etfw\" class=\"twitter-follow-button\" data-show-count=\"true\">Follow @tylersnetwork</a><script async src=\"https://platform.twitter.com/widgets.js\" charset=\"utf-8\"></script>" index.html
+line=57
+gsed -i "${line}i <a href=\"https://twitter.com/tylersnetwork?ref_src=twsrc%5Etfw\" class=\"twitter-follow-button\" data-show-count=\"true\">Follow @tylersnetwork</a><script async src=\"https://platform.twitter.com/widgets.js\" charset=\"utf-8\"></script>" index.html
 
 # Deleting ~ files
 rm *~
